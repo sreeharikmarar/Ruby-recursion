d4ad2adc9ba5fb67e9ca778af2f1758cbba7a748
prescience <prescience@prescience-laptop.(none)>
2012-12-11 18:14:24 +0530

    second file has been added

--- fact.rb
+++ fact.rb
@@ -1,15 +1,11 @@
 class Fact
-  attr_accessor :num
-  def initialize(num)
-    @num = num
-  end
-  def fact
-    return 1 if @num == 0 
-    return @num * Fact.new(self.num - 1).fact
+  def fact(n)
+    return 1 if n == 0
+    return n * fact(n-1)
   end
 end
 
 puts "Enter the number that you want to calculate factorial"
 num = gets
-print Fact.new(num.to_i).fact
+print Fact.new.fact(num.to_i)
 
---
+++ fact2.rb
@@ -0,0 +1,15 @@
+class Fact
+  attr_accessor :num
+  def initialize(num)
+    @num = num
+  end
+  def fact
+    return 1 if @num == 0 
+    return @num * Fact.new(self.num - 1).fact
+  end
+end
+
+puts "Enter the number that you want to calculate factorial"
+num = gets
+print Fact.new(num.to_i).fact
+
